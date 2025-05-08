e59e7f6edfef7578923362d6a9420c0191e49347
kavi-thagadoor <kaviarasan.m@thagadoor.org>
2025-05-08 15:57:29 +0530

    init

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
+
+io.on('connection', socket => {
+  console.log('Client connected:', socket.id);
+
+  socket.on('send_data', (data) => {
+    console.log('Data received from web:', data);
+    io.emit('receive_data', data); // send to all clients
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
